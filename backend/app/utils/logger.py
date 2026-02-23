cd0f79b7ee9b4267887b55ec98e274230752672a
anass17 <anassboutaib2018@gmail.com>
2026-02-23 12:11:06 +0000

    Create project structure

---
+++ backend/app/utils/logger.py
@@ -0,0 +1,21 @@
+"""
+Logger utility
+"""
+import logging
+import sys
+
+
+def setup_logger(name):
+    """Setup logger for the application"""
+    logger = logging.getLogger(name)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    
+    return logger
